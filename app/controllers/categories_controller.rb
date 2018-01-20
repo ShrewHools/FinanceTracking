ad423e912dfca141a72c14bc3b40914093a932c0
shrewhools <vladyukha@yandex.ru>
2018-01-21 02:52:36 +0300

    Add update category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,21 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = current_user.categories.find_by(id: params[:id])
+    redirect_to categories_path, flash: { danger: 'Category not found' } unless @category
+  end
+
+  def update
+    @category = current_user.categories.find_by(id: params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to categories_path, flash: { success: 'Category updated' }
+    else
+      flash.now[:danger] = @category.errors.values.flatten
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params

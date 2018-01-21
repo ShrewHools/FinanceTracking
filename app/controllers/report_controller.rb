14e1b10baf7ceb5917314b02a608aa7a2921dfac
shrewhools <vladyukha@yandex.ru>
2018-01-21 13:14:28 +0300

    Add report page

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,4 +1,8 @@
 class ReportController < ApplicationController
   def index
+    if current_user
+      @expenses = current_user.expenses.order(when: :desc)
+      @incomes = current_user.incomes.order(when: :desc)
+    end
   end
 end

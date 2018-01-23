f535473361f53931b49f6ca3c6092dea939af8c1
shrewhools <vladyukha@yandex.ru>
2018-01-23 21:02:51 +0300

    Change amount type float to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121054816) do
+ActiveRecord::Schema.define(version: 20180123175827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,25 +25,25 @@ ActiveRecord::Schema.define(version: 20180121054816) do
   end
 
   create_table "expenses", force: :cascade do |t|
-    t.float    "amount"
+    t.decimal  "amount",      precision: 10, scale: 2
     t.date     "when"
     t.text     "description"
     t.integer  "user_id"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.index ["category_id"], name: "index_expenses_on_category_id", using: :btree
     t.index ["user_id"], name: "index_expenses_on_user_id", using: :btree
   end
 
   create_table "incomes", force: :cascade do |t|
-    t.float    "amount"
+    t.decimal  "amount",      precision: 10, scale: 2
     t.date     "when"
     t.text     "description"
     t.integer  "user_id"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.index ["category_id"], name: "index_incomes_on_category_id", using: :btree
     t.index ["user_id"], name: "index_incomes_on_user_id", using: :btree
   end
